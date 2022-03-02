26e3aebce4156b164ada2182fccc517caa969513
dathq <dathq@paxcreation.com>
2022-03-02 22:51:47 +0700

    Add html input element

--- components/forms/input.tsx
+++ components/forms/input.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 const Input: NextPage = () => {
   return (
     <div className={styles.container}>
-
+      <input type="text" value="" />
     </div>
   )
 }
